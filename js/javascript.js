16e59f117bc6af4586137875562e92fdaac984f2
Aluno <aluno@estacio.acad>
2017-08-14 21:37:05 -0300

    atulização

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,8 @@
 
-var numero1 = parseInt (prompt('Informe o primeiro numero: '));
-var numero2 = parseInt (prompt('Informe o segundo  numero: '));
+var numero1 = parseInt (prompt("Informe o primeiro numero"));
+var numero2 = parseInt (prompt("Informe o segundo  numero: "));
+
+alert("Adição= " + (numero1+numero2 )+"\n" +
+      "Subtração= " + (numero1-numero2 )+"\n" +
+      "Multiplicação= " + (numero1*numero2 )+"\n" +
+      "Divisão= " + (numero1/numero2 )+"\n"  );
