47495a2e5aa94cc67926d7360532d15ce9007ee6
IsraelPassos <israelpassos30@gmail.com>
2017-08-21 12:26:56 -0300

    atualização

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,10 @@
 var numero1 = parseInt (prompt("Informe o primeiro numero"));
 var numero2 = parseInt (prompt("Informe o segundo  numero: "));
 
-alert("Adição= " + (numero1+numero2 )+"\n" +
-      "Subtração= " + (numero1-numero2 )+"\n" +
-      "Multiplicação= " + (numero1*numero2 )+"\n" +
-      "Divisão= " + (numero1/numero2 )+"\n"  );
+alert("Adicao =        " + (numero1+numero2 )+"\n" +
+      "Subtracao =     " + (numero1-numero2 )+"\n" +
+      "Multiplicacao = " + (numero1*numero2 )+"\n" +
+      "Divisao =       " + (numero1/numero2 )+"\n"  );
+
+var nome = 'ISRAEL PASSOS';
+alert(nome);
